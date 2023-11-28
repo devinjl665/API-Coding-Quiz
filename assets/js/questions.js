ed45e1ada2995fcb08496b5138aeb0ad6ff166d5
elroy <devinjl665@gmail.com>
2023-11-28 10:35:15 -0600

    made adjustment to script.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,26 +1,26 @@
-var questions = [
+var quizQuestions = [
     {
         question: "What do you use to declare a variable?",
         answers: ["var", "let", "const", "all of the above"],
-        correct: 4,
+        correct: 3,
     },
 
     {
         question: "What is the difference between '==' and '==='?",
         answers: ["There is no difference", "An extra equal sign", "'==' only checks for equality in value", "'==' only checks for absolute values"],
-        correct: 3,
+        correct: 2,
     },
 
     {
         question: "What are the three types of pop up boxes?",
         answers: ["alerts, text, and inputs", "alerts, confirms, and prompts", "notifications, confirms, and uhhhhhh", "none of the above"],
-        correct: 2,
+        correct: 1,
     },
 
     {
         question: "An undefined value means everything BUT?",
         answers: ["the property is in a string and needs to be parsed", "the variable used in the code doesn't exist", "the variable is not assigned to any value", "the property does not exist"],
-        correct: 1,
+        correct: 0,
     },
 
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var startScreen = document.querySelector('#start-screen')
 var startButton = document.querySelector('#start');
-var questionsTitle = document.querySelector('#question-title')
+var questionTitle = document.querySelector('#question-title')
 var questions = document.querySelector('#questions');
 var choices = document.querySelector('#choices');
 var time = document.querySelector('#time');
@@ -11,8 +11,8 @@ var saveButton = document.querySelector('#submit');
 var feedback = document.querySelector('#feedback');
 
 //variables
-let currentQuestionIndex = 0;
-let timer = 0;
+let questionIndex = 0;
+let countdownTimer = 0;
 let orderList = document.createElement('ol');
 choices.appendChild(orderList);
 let ol = document.querySelector('ol');
@@ -21,21 +21,36 @@ let ol = document.querySelector('ol');
 ol.addEventListener('click', function (event) {
     let element = event.target;
     if (element.matches('button')) {
-        let state = element.getAttribute('data-state');
-        if (state == questions[currentQuestionIndex].correct) {
+        let selectedState = element.getAttribute('data-state');
+        let correctAnswerIndex = quizQuestions[questionIndex].correct;
+
+        console.log('Selected State:', selectedState);
+        console.log('Correct Answer Index:', correctAnswerIndex.toString());
+
+        if (selectedState === correctAnswerIndex.toString()) {
             message('Correct!');
         } else {
             message('Wrong!');
             countdownTimer -= 10;
             console.log(countdownTimer);
         }
-        currentQuestionIndex++;
-        ol.textContent = '';
-        showQuestion(currentQuestionIndex);
+
+        questionIndex++;
+
+        if (questionIndex < quizQuestions.length) {
+        // Show the next question
+            ol.textContent = '';
+            showQuestion(questionIndex);
+        } else {
+        // Quiz has ended, show final score
+            endQuiz();
+        }
     }
 });
 
-function startQuiz() {}
+
+
+//
 function message(string) {
   console.log(string);
   let p1 = document.createElement("p");
@@ -50,47 +65,57 @@ function message(string) {
   }, 1000);
 }
 
+var intervalId;
+
+//
 function startCountdown() {
-  timer = 60;
-  time.textContent = timer;
+  countdownTimer = 60;
+  time.textContent = countdownTimer;
   let intervalId = setInterval(function () {
-    timer--;
-    time.textContent = timer;
-    console.log(timer);
-    if (timer > 0 && currentQuestionIndex >= questions.length) {
+    countdownTimer--;
+    time.textContent = countdownTimer;
+    console.log(countdownTimer);
+    if (countdownTimer > 0 && questionIndex >= quizQuestions.length) {
       clearInterval(intervalId);
       time.textContent = countdownTimer;
       questions.setAttribute("class", "hide");
       finishScreen.setAttribute("class", "start");
-      finalScore.textContent = timer;
+      finalScore.textContent = countdownTimer;
     }
-    if (timer <= 0) {
+    if (questionIndex >= quizQuestions.length) {
       clearInterval(intervalId);
-      time.textContent = 0;
-      questions.setAttribute("class", "hide");
-      finishScreen.setAttribute("class", "start");
-      finalScore.textContent = 0;
+      endQuiz();
     }
   }, 1000);
 }
 
+function endQuiz() {
+    // Display the final score and perform any other necessary actions
+        questions.setAttribute('class', 'hide');
+        finishScreen.setAttribute('class', 'start');
+        finalScore.textContent = countdownTimer;
+    
+    // Clear the countdown interval
+        clearInterval(intervalId);    
+    }
+
 // Function to display a question
 function showQuestion(index) {
-    if (index < questions.length) {
-        questionsTitle.textContent = questions[index].question;
+    if (index < quizQuestions.length) {
+        questionTitle.textContent = quizQuestions[index].question;
         console.log(index);
-        console.log(questions.length);
-        for (let i = 0; i < questions[index].answers.length; i++) {
+        console.log(quizQuestions.length);
+        for (let i = 0; i < quizQuestions[index].answers.length; i++) {
             let li = document.createElement('li');
             let selectOlist = document.querySelector('ol');
             selectOlist.appendChild(li);
             let buttonAnswer = document.createElement('button');
             buttonAnswer.setAttribute('data-state', i);
-            buttonAnswer.textContent = questions[index].answers[i];
+            buttonAnswer.textContent = quizQuestions[index].answers[i];
             li.appendChild(buttonAnswer);
         }
-    } else {
-        questionsTitle.textContent = '';
+    }   else {
+        questionTitle.textContent = '';
     }
 }
 
@@ -98,7 +123,7 @@ startButton.addEventListener("click", function () {
     startCountdown();
     startScreen.setAttribute("class", "hide");
     questions.setAttribute("class", "");
-    showQuestion(currentQuestionIndex);
+    showQuestion(questionIndex);
   });
   
   let highscore = JSON.parse(localStorage.getItem("scores")) || [];
@@ -107,7 +132,7 @@ startButton.addEventListener("click", function () {
     let initialStore = inputName.value;
     let currentScore = {
       initial: initialStore,
-      score: timer,
+      score: countdownTimer,
     };
     highscore.push(currentScore);
     console.log(currentScore);
